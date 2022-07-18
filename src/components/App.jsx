587bd52ca5a0ee533e274d7bd35afa824721e3e1
pstergiou <petrosstergiou0@gmail.com>
2022-07-18 18:52:36 +0300

    Part2- added more functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Note from '../components/Note';
+import notes from "../notes";
 
 function App(){
     return (<div><Header></Header>
-    <Note></Note>
+    {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
     <Footer></Footer> </div>);
 }
 
